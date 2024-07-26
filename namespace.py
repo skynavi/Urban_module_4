f89e8ebdc9be834b6d13567b4153c7c6aa8aacbb
skynavi <skynavi@mail.ru>
2024-07-26 23:07:21 +0300

    2608

--- namespace.py
+++ namespace.py
@@ -2,7 +2,7 @@ def test_function():
     def inner_function():
         print("Я в области видимости функции test_function")
 
-    inner_function() #вызываем функцию внутри функции
+    inner_function() #вызываем функцию внутри функции.
 
 test_function()
 
